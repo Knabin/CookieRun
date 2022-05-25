bdd5e6370d2fb2aef38fc6554be4c219ce1ff435
Knabin <knabinn97@gmail.com>
2022-05-25 22:20:51 +0900

    GameScene 정리
    * UI 구조 변경.
    * 기본 쿠키를 용감한 쿠키로 변경.

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    [SerializeField]
+    GameObject StartUI;
+
     void Start()
     {
         base.Init();
@@ -16,8 +19,14 @@ public class GameScene : BaseScene
         // TODO :
         // 맵 로드가 끝나면 UI 끄고, 게임 시작 처리
         // 시작 전까지는 게임 속도를 0으로 둔다
+        StartCoroutine(StartGame());
     }
 
+    IEnumerator StartGame() {
+        yield return new WaitForSeconds(5.0f);
+
+        StartUI.SetActive(false);
+    }
 
     public override void Clear()
     {
